d1a36f1384dc3839ba0704d14ed4fc4ac31d7885
William Weisberger <bill.weisberger@gmail.com>
2019-07-25 20:41:46 -0600

    created file structure and setup db and server.js. Both are running according to console logs.

---
+++ db/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const connectionString = "mongodb://localhost/dtp";
+mongoose.connect(connectionString, {useNewUrlParser: true});
+
+mongoose.connection.on("connected", ()=>{
+    console.log(`mongoose connected on  ${connectionString}`);
+});
+
+mongoose.connection.on("disconnected", ()=>{
+    console.log(`mongoose disconnected from  ${connectionString}`);
+});
+
+mongoose.connection.on("error", (err)=>{
+    console.log(`mongoose error:  ${err}`);
+});
